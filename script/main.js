4820928dc87a154321504ebc56c0bd1ab0f097f7
Marcos Vinicius <avantiermv@gmail.com>
2025-06-11 18:02:54 -0300

    I started the js code
    I just tested some things in the code.

---
+++ script/main.js
@@ -0,0 +1,13 @@
+class generateTicket{
+    constructor(name, email, githubnName){
+        
+    }
+}
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    console.log("Rapaz, ele tá sem zap");
+});
+
---
+++ script/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+or('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    console.log("Rapaz, ele tá sem zap");
