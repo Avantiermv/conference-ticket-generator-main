43c4bd80ccf98a7880f15c74a73e867d9924010f
Marcos Vinicius <avantiermv@gmail.com>
2025-06-13 18:58:02 -0300

    .

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,24 +7,16 @@ class generateTicket{
 
 const form = document.querySelector('form');
 const divContent = document.querySelector('.content');
-const divLoading = document.querySelector('.loading');
+
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
     divContent.classList.add('hidden');
+
     setTimeout(() => {
       divContent.style.display = 'none';
     }, 500)
 });
 
-document.addEventListener('DOMContentLoaded', () =>{
-    lottie.loadAnimation({
-        container: document.getElementById("animation-container"),
-        renderer: "svg",
-        loop: true,
-        autoplay: true,
-        path: "assets/script/loadinganimation3.json"
-    });
-})
 
 
 
