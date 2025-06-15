1c10f386aa614b71e3590447c453f0a5f3959286
Marcos Vinicius <avantiermv@gmail.com>
2025-06-15 09:36:45 -0300

    LE LE LE LE MANS!!!♥️
    FORZA FERRARI!!!

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,13 +7,19 @@ class generateTicket{
 
 const form = document.querySelector('form');
 const divContent = document.querySelector('.content');
+const divGeneratedTicket = document.querySelector('.generate-ticket');
+const spanFullName = document.querySelector('span');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
     divContent.classList.add('hidden');
+    divGeneratedTicket.classList.remove('hidden');
+    divGeneratedTicket.classList.add('visible');
+    spanFullName.innerText = 'Marcos Vinicius silva colares';
 
     setTimeout(() => {
       divContent.style.display = 'none';
+      divGeneratedTicket.style.display = 'flex';
     }, 500)
 });
 
